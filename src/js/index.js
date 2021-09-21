a17b673739f55de0bd4089b3d0ac78fb7e5934b1
Monika Sarnecka <monika.sarnecka@interia.pl>
2021-09-21 17:51:54 +0200

    homework

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,30 @@ import '../scss/main.scss';
 /* place your code below */
 
 console.log('HELLO 🚀')
+
+let isDark=false;
+
+const button = document.querySelector('.action--js');
+console.log(button)
+
+button.addEventListener('click', () => {
+    if (isDark)
+    {document.documentElement.style.setProperty('--background-color','black');
+    document.documentElement.style.setProperty('--text-color','white');
+    isDark=false;}
+
+    else 
+    {
+        document.documentElement.style.setProperty('--background-color','white');
+        document.documentElement.style.setProperty('--text-color','black');
+        isDark=true;
+    }
+
+})
+
+
+
+
+
+
+
--- webpack.common.js
+++ webpack.common.js
@@ -88,5 +88,11 @@ module.exports = {
       chunks: ["index", "another"],
       filename: "another.html",
     }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/monika.html",
+      inject: true,
+      chunks: ["index", "another"],
+      filename: "monika.html",
+    }),
   ],
 };
